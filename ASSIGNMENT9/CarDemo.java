acb1fd6bca28d5287e6b88895db64e7fad025713
harivarshannn <harivarshan2006@outlook.com>
2024-09-06 23:08:37 +0530

    updated CarDemo.java

--- ASSIGNMENT9/CarDemo.java
+++ ASSIGNMENT9/CarDemo.java
@@ -1,18 +1,15 @@
 public class CarDemo {
 
-    // Inner Car class
+    
     static class Car {
-        // Fields
         private String make;
         private String model;
 
-        // Constructor
         public Car(String make, String model) {
             this.make = make;
             this.model = model;
         }
 
-        // Method to display car details
         public void displayCarInfo() {
             System.out.println("Make: " + make);
             System.out.println("Model: " + model);
@@ -20,10 +17,7 @@ public class CarDemo {
     }
 
     public static void main(String[] args) {
-        // Create an object of the Car class
         Car myCar = new Car("Toyota", "Camry");
-
-        // Call method to display car details
         myCar.displayCarInfo();
     }
 }
