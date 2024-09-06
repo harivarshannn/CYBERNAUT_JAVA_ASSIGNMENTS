70187c93176b2f1a214289ea8f0138c98bfa40c2
harivarshannn <harivarshan2006@outlook.com>
2024-09-06 23:10:53 +0530

    SalaryCasting.java

---
+++ DAY3/SalaryCasting.java
@@ -0,0 +1,7 @@
+public class SalaryCasting {
+    public static void main(String[] args) {
+        double salary = 75000.50;
+        int salaryAsInt = (int) salary;
+        System.out.println("The salary as an integer is: " + salaryAsInt);
+    }
+}
