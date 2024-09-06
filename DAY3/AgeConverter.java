2875d4165ed6563045191c7bf7777183db4543ef
harivarshannn <harivarshan2006@outlook.com>
2024-09-06 23:10:03 +0530

    updated Ageconverter.java

---
+++ DAY3/AgeConverter.java
@@ -0,0 +1,7 @@
+public class AgeConverter {
+    public static void main(String[] args) {
+        int age = 25;
+        String ageString = Integer.toString(age);
+        System.out.println("Age: " + ageString);
+    }
+}
