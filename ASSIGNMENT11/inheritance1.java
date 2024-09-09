0483f00af58a1888850a328fe03e878a956b9e51
harivarshannn <harivarshan2006@outlook.com>
2024-09-09 21:45:36 +0530

    added inheritance1.java

---
+++ ASSIGNMENT11/inheritance1.java
@@ -0,0 +1,22 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal makes a sound.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        myAnimal.makeSound();
+
+        Dog myDog = new Dog();
+        myDog.makeSound();
+    }
+}
