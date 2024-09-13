2ef08561e6ada099e07066364df09155009995b6
harivarshannn <harivarshan2006@outlook.com>
2024-09-13 22:37:28 +0530

    modified DivideByZeroError.java

---
+++ ASSIGNMENT14/DivideByZeroError.java
@@ -0,0 +1,9 @@
+public class DivideByZeroError {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
