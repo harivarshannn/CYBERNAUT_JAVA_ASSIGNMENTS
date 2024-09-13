c48e6eefaeefb315af42871fca3ebefa39d04c4c
Harivarshan N <harivarshan2006@outlook.com>
2024-09-13 22:39:16 +0530

    Delete ASSIGNMENT15 directory

--- ASSIGNMENT15/DivideByZeroError.java
+++
@@ -1,9 +0,0 @@
-public class DivideByZeroError {
-    public static void main(String[] args) {
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero");
-        }
-    }
-}
