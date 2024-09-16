9d5dc949972a770a3505016d648071dab7556d8a
harivarshannn <harivarshan2006@outlook.com>
2024-09-16 21:01:25 +0530

    added Author.java

---
+++ JAVA_PRACTICAL/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+    public String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
